54ac2a6bfb3fc85d7b2cbd297e19c67c444ae377
dressrahmat <dedysrahmat97@gmail.com>
2024-05-26 16:57:45 +0700

    mengubah route welcome langsung ke login

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ use App\Http\Controllers\Dashboard\ImportController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 Route::middleware([
